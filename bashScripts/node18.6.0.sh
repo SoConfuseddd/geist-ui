41a840d601ff63ece9aba71cc56181f6e7d11f7c
SoConfuseddd <reactuilibrarytask@gmail.com>
2024-11-24 18:50:06 +0200

    Update node18.6.0.sh

--- bashScripts/node18.6.0.sh
+++ bashScripts/node18.6.0.sh
@@ -3,17 +3,17 @@
 
 #Installing Node version 18.6.0
 
-curl -O https://nodejs.org/dist/v18.6.0/node-v18.6.0-linux-x64.tar.xz
-tar -xf node-v18.6.0-linux-x64.tar.xz
+curl -O https://nodejs.org/dist/v20.18.0/node-v20.18.0-linux-x64.tar.xz
+tar -xf node-v20.18.0-linux-x64.tar.xz
 
-sudo mv node-v18.6.0-linux-x64 /usr/local/node-v18.6.0
+sudo mv node-v20.18.0-linux-x64 /usr/local/node-v20.18.0
 
-sudo ln -s /usr/local/node-v18.6.0/bin/node /usr/bin/node
-sudo ln -s /usr/local/node-v18.6.0/bin/npm /usr/bin/npm
+sudo ln -s /usr/local/node-v20.18.0/bin/node /usr/bin/node
+sudo ln -s /usr/local/node-v20.18.0/bin/npm /usr/bin/npm
 
 npm install -g yarn
 
-sudo echo "export PATH="$PATH:/usr/local/node-v18.6.0/bin"" >> ~/.bashrc
+sudo echo "export PATH="$PATH:/usr/local/node-v20.18.0/bin"" >> ~/.bashrc
 
 
 source ~/.bashrc
